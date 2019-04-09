ed2985ebfd211a0c89f7546070a60aa12fe826ff
Nguyen Hoang Son <nhson@apcs.vn>
2019-04-10 02:08:21 +0700

    update docker

--- database/init-db.sh
+++ database/init-db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE efeed;
-  CREATE DATABASE efeed_test;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER efeed;
+    CREATE DATABASE efeed;
+    GRANT ALL PRIVILEGES ON DATABASE efeed TO efeed;
 EOSQL
